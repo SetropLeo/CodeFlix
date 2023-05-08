05c7b9a50b4a7b15a96012cea12de6ffb743c800
SetropLeo <leol_portes@hotmail.com>
2023-05-08 19:22:04 -0300

    Revert "Feature/header component"

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,58 @@
-import { Box, ThemeProvider, createTheme } from '@mui/material';
 import React from 'react';
-import { Header } from './components';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
 
-const theme = createTheme({});
-
-const App = () => {
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Box
-        component="main"
-        sx={{
-          height: '100vh',
-          backgroundColor: '#000',
-        }}
-      >
-        <Header />
-      </Box>
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
